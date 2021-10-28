cec4cab99a0b16fa4da029b329904ecfec4b35d9
MLGTASTICa <ak9bc01d@yahoo.com>
2021-10-28 17:21:55 +0300

    Update SharedMobSanityComponent.cs

--- Content.Shared/Sanity/SharedMobSanityComponent.cs
+++ Content.Shared/Sanity/SharedMobSanityComponent.cs
@@ -4,6 +4,12 @@ using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 using Robust.Shared.Players;
 using Robust.Shared.Network;
+using Robust.Shared.Prototypes;
+using Robust.Shared.ViewVariables;
+using System.Collections.Generic;
+using Content.Shared.Construction.Conditions;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Utility;
 
 namespace Content.Shared.Sanity
 {
@@ -13,7 +19,19 @@ namespace Content.Shared.Sanity
     }
 
     [Serializable, NetSerializable]
-    public enum SanityUiKey
+
+    [Prototype("SanityBreakdown")]
+    public class SanityBreakdownPrototype : IPrototype
+    {
+        [DataField("name")]
+        public string Name { get; } = string.Empty;
+
+        [DataField("id", required: true)]
+        public string ID { get; } = default!;
+
+        [DataField("duration")]
+        public float Duration { get; } = 120.0f;
+        public enum SanityUiKey
     {
         Key,
     }

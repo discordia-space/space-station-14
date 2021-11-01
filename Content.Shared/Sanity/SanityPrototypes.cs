e81a6573fda92bd00c6d2175aa1569cc2deb5e80
MLGTASTICa <ak9bc01d@yahoo.com>
2021-11-01 19:49:50 +0200

    Part 18 ? : Breakdown prototypes

--- Content.Shared/Sanity/SanityPrototypes.cs
+++ Content.Shared/Sanity/SanityPrototypes.cs
@@ -19,17 +19,46 @@ namespace Content.Shared.Sanity.Prototypes
     public class SanityBreakdownPrototype : IPrototype
     {
         [DataField("name")]
-        public string Name { get; } = string.Empty;
+        public ISanityBreakdown Name { get; } = default!;
 
-        [DataField("id", required: true)]
+        [DataField("ID", required: true)]
         public string ID { get; } = default!;
 
         [DataField("duration")]
         public float Duration { get; } = 120.0f;
 
+        [DataField("canoccurat")]
+
+        public int CanOccurAt { get; } = 30;
+
+        [DataField("imposeddelay")]
+
+        public float ImposedDelay { get; } = 300.0f;
+
+        [DataField("beginstring")]
+        public string BeginString { get; } = string.Empty;
+
+        [DataField("endingstring")]
+
+        public string EndingString { get; } = string.Empty;
+
+        [DataField("reoccuring")]
+
+        public bool Reoccuring { get; } = false;
+
+        [DataField("reoccuringmessage")]
+
+        public float ReoccuringInterval { get; } = 10.0f;
+
+        [DataField("reoccuringeffects")]
+
+        public ISanityReoccuring ReoccuringEffects { get; } = default!;
+
+
+
     }
 
-    public interface IBreakdown
+    public interface ISanityBreakdown
     {
         void Start(SanityBreakdownEventArgs args)
         {
@@ -37,6 +66,19 @@ namespace Content.Shared.Sanity.Prototypes
 
         void End(SanityBreakdownEventArgs args)
         {
+        }
+    }
+
+    public interface ISanityReoccuring
+    {
+        void ApplyEffects(SanityBreakdownEventArgs args)
+        {
+
+        }
+
+        void EndEffects(SanityBreakdownEventArgs args)
+        {
+
         }
     }
 }
